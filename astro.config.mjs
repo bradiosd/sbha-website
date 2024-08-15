99eec6bed55abf7baa54eb76c6e01f21803e1c05
bradiosd <brad@128bitstudios.co.uk>
2024-08-15 22:20:55 +0100

    fix sitemap and links

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,30 +12,12 @@ export default defineConfig({
   },
   integrations: [
     sitemap({
-      filter: (page) =>
-        page !== `${site}/home` &&
-        page !== `${site}/smart-homes` &&
-        page !== `${site}/smart-homes/lightning` &&
-        page !== `${site}/smart-homes/energy` &&
-        page !== `${site}/smart-homes/hvac` &&
-        page !== `${site}/smart-homes/displays` &&
-        page !== `${site}/smart-homes/automation` &&
-        page !== `${site}/security` &&
-        page !== `${site}/security/cctv` &&
-        page !== `${site}/security/doors-and-windows` &&
-        page !== `${site}/security/motion-detection` &&
-        page !== `${site}/security/flood-detection` &&
-        page !== `${site}/security/gas-detection` &&
-        page !== `${site}/security/smoke-detection` &&
-        page !== `${site}/networking` &&
-        page !== `${site}/about-us` &&
-        page !== `${site}/privacy-policy` &&
-        page !== `${site}/terms-and-conditions`
+      //
     }),
     partytown({
-        config: {
-          forward: ["dataLayer.push"],
-        },
+      config: {
+        forward: ["dataLayer.push"],
+      },
     }),
   ],
 });
