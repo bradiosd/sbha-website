297b84f400287957c4de91a75bb3a425e66aaf89
bradiosd <brad@128bitstudios.co.uk>
2024-08-22 12:54:43 +0100

    contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown'
+import vercel from '@astrojs/vercel/serverless';
 
 const site = 'https://smartbirdha.co.uk'
 
 // https://astro.build/config
 export default defineConfig({
   site,
+  output: 'server',
+  adapter: vercel(),
   redirects: {
     '/': '/home'
   },
